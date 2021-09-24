3e757fbcd6ccbd00614b06735098f1e6f264ae24
Nicolas <nicolas.cabezas@davinci.edu.ar>
2021-09-24 10:18:34 -0300

    Proyecto nuevo

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, FlatList } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  constructor() 
+  {
+    super();
+    this.state ={
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    this.apiCall();
+  }
+  async apiCall() 
+  {
+    let resp =await fetch('https://pokeapi.co/api/v2/pokemon?offset=20&limit=20')
+    let respJson= await resp.json()
+    //console.warn(respJson)
+    this.setState({data:respJson.results})
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={{ fontSize: 50, padding: 50, backgroundColor:'red' }}>API POKEMON</Text>
+        <FlatList
+        data={this.state.data}
+        renderItem={({item}) =>
+        <Text style= {{fontSize: 20, backgroundColor: 'yellow', margin:5}}>{item.name}</Text>
+      }
+        />
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;

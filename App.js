9afcf98ae774f4e36c02c94f0218b99c0eabc88b
nicocabezas <nicolas.cabezas@davinci.edu.ar>
2021-12-05 09:43:02 -0300

    drawerMenu + Api

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import Fetch from './screens/Fetch';
+import HomeScreen from './screens/Home';
 
 const Drawer = createDrawerNavigator();
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+HomeScreen();
 
 function AboutScreen() {
   return (
@@ -22,12 +17,15 @@ function AboutScreen() {
 }
 
 
+
+
 function App() {
  return (
    <NavigationContainer>
      <Drawer.Navigator initialRouteName="Home">
        <Drawer.Screen name="Home" component={HomeScreen} />
        <Drawer.Screen name="About" component={AboutScreen} />
+       <Drawer.Screen name="Fetch" component={Fetch} />
      </Drawer.Navigator>
    </NavigationContainer>
  );
--- screens/Fetch.js
+++ screens/Fetch.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import {StyleSheet, View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+    StyleSheet,
+    Text,
+    SafeAreaView,
+    FlatList,
+    ActivityIndicator,
+} from 'react-native';
 
-function Fetch () {
-    return(
-    <View style= { styles.container}>
+const movieURL = "https://reactnative.dev/movies.json";
 
-        <Text style={styles.titleText}>Fetch de la app Pokemon</Text>
-    </View>
+const Fetch = () => {
+
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch(movieURL)
+            .then((response) => response.json())
+            .then((json) => setData(json.movies))
+            .catch((error) => alert(error))
+            .finally(() => setLoading(false));
+    }, []);
+
+    return (
+        // <text style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>hola</text>
+        <SafeAreaView style={styles.container}>
+            {isLoading ? (
+                <ActivityIndicator />
+            ) : (
+                <FlatList
+                    data={data}
+                    keyExtractor={({ id }, index) => id}
+                    renderItem={({ item }) => (
+                        <Text>
+                            {item.title}
+                            {item.releaseYear}
+                        </Text>
+                    )}
+                />
+            )}
+            
+        </SafeAreaView>
     )
-}
+};
+
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         padding: 24
     },
     titleText: {
